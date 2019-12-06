3fcc592d92dfe3ad911682ed0b224a2204bb4bb3
Dhrubojyoti Roy <royd@cse.osu.edu>
2019-12-06 02:02:05 -0500

    Adding tvt split arffs and generation script

--- arff_to_tvt_split.py
+++ arff_to_tvt_split.py
@@ -6,14 +6,19 @@ import numpy as np
 import pandas as pd
 
 # Load arff into dataframe
-filename = sys.argv[1]
+filename = '1006_f_465_humans_490_bikes.arff'
 data = loadarff(filename)
-df = pd.DataFrame(data[0])
+
+
+df = pd.DataFrame(data[0]).astype({'Target Count_1005': 'int'})
+
+# Convert label into string from bytestring
+df['classLabel_1006'] = df['classLabel_1006'].str.decode("utf-8")
 
 # Create train, val, test arffs
-train_indices = np.loadtxt('train_indices.txt', dtype=int)-1
-val_indices = np.loadtxt('val_indices.txt', dtype=int)-1
-test_indices = np.loadtxt('test_indices.txt', dtype=int)-1
+train_indices = np.loadtxt('train_indices.txt', dtype=int)-1  # 0-indexing
+val_indices = np.loadtxt('val_indices.txt', dtype=int)-1      # 0-indexing
+test_indices = np.loadtxt('test_indices.txt', dtype=int)-1    # 0-indexing
 
 data_train = df.iloc[train_indices]
 data_val = df.iloc[val_indices]
@@ -21,17 +26,17 @@ data_test = df.iloc[test_indices]
 
 relname = data[1].name
 
-dump(os.path.join(os.path.dirname(filename), 'train.arff')
+dump(os.path.join(os.path.dirname(filename), 'train_' + filename)
       , data_train.values
       , relation=relname + '_train'
       , names=df.columns)
 
-dump(os.path.join(os.path.dirname(filename), 'val.arff')
+dump(os.path.join(os.path.dirname(filename), 'val_' + filename)
       , data_val.values
       , relation=relname + '_val'
       , names=df.columns)
 
-dump(os.path.join(os.path.dirname(filename), 'test.arff')
-      , data_val.values
+dump(os.path.join(os.path.dirname(filename), 'test_' + filename)
+      , data_test.values
       , relation=relname + '_test'
       , names=df.columns)
