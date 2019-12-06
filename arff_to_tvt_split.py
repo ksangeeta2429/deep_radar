8fb24f8cb08726430e16ffd588848852432c46b8
Dhrubojyoti Roy <roy.174@buckeyemail.osu.edu>
2019-12-06 14:35:06 -0500

    Splitting file order finton TVT

--- arff_to_tvt_split.py
+++ arff_to_tvt_split.py
@@ -5,6 +5,12 @@ import os
 import numpy as np
 import pandas as pd
 
+def extract_list_to_file(in_list, line_indices, outfile):
+      outlist = [ in_list[i] for i in line_indices ]
+
+      with open(outfile, 'w') as filehandle:
+            filehandle.writelines("%s" % y for y in outlist)
+
 # Load arff into dataframe
 filename = '1006_f_465_humans_490_bikes.arff'
 data = loadarff(filename)
@@ -26,6 +32,7 @@ data_test = df.iloc[test_indices]
 
 relname = data[1].name
 
+# Write train, valid, test files
 dump(os.path.join(os.path.dirname(filename), 'train_' + filename)
       , data_train.values
       , relation=relname + '_train'
@@ -40,3 +47,15 @@ dump(os.path.join(os.path.dirname(filename), 'test_' + filename)
       , data_test.values
       , relation=relname + '_test'
       , names=df.columns)
+
+# Write train, val, test file orders
+fileorder = '1006_f_465_humans_490_bikes_file_order.txt'
+with open(fileorder) as f:
+      lines = f.readlines()
+
+lines = [ x for x in lines if "Metadata" not in x] # Delete lines containing "Metadata"
+assert len(lines)==len(df)
+
+extract_list_to_file(lines, train_indices, os.path.join(os.path.dirname(fileorder), 'train_' + fileorder))
+extract_list_to_file(lines, val_indices, os.path.join(os.path.dirname(fileorder), 'val_' + fileorder))
+extract_list_to_file(lines, test_indices, os.path.join(os.path.dirname(fileorder), 'test_' + fileorder))
